47d733818d8af2ea82b56acf4457205ae257cd6b
Alex Woodall <aww2539@gmail.com>
2021-07-15 16:44:04 -0500

    Learned interpolated strings

--- notes.js
+++ notes.js
@@ -1,26 +1,44 @@
 const notes = [
     {
         id: 1,
-        subject: "Arrays and Objects",
+        subject: "arrays and objects",
         date: "July 15th",
-        feeling: "So far so good",
-        timeSpect: "2 hours",
+        feeling: "so far so good",
+        timeSpent: "2 hours",
     },
 ]
 
 const js2 = {
-    id: 1,
-    subject: "Pushing into arrays",
+    id: 2,
+    subject: "pushing into arrays",
     date: "July 15th",
-    feeling: "Moving along well",
-    timeSpect: "1 hour", 
+    feeling: "moving along well",
+    timeSpent: "1 hour", 
+}
+
+const js3 = {
+    id: 3,
+    subject: "interpolated strings",
+    date: "July 15th",
+    feeling: "very good",
+    timeSpent: "30 minutes",
+}
+
+const js4 = {
+    id: 4,
+}
+
+const js5 = {
+    id: 5,
 }
 
 notes.push(js2)
+notes.push(js3)
 
 for (const note of notes) {
     console.log(note.subject);
     console.log(note.date);
-    console.log(note.feeling);
+    console.log(note.timeSpent);
+    console.log(`On ${note.date}, I learned ${note.subject} in about ${note.timeSpent}.`)
     console.log('')
 }
